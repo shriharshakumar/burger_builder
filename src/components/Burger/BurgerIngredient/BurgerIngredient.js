c535b919b0008bd91b8da1370b41d9452ef32c17
shriharshakumar <shriharshakumar@gmail.com>
2018-08-18 21:59:08 +0530

    Added burger styling...

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
+import classes from './BurgerIngredient.css';
 
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
-    render(){
+    render () {
 
         let ingredient = null;
 
         switch(this.props.type){
             case('bread-bottom'):
-                ingredient = <div className='classes.BreadBottom'></div>;
+                ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ('bread-top'):
                 ingredient = (
-                    <div className='classes.BreadTop'>
-                        <div className='classes.Seeds1'></div>
-                        <div className='classes.Seeds2'></div>
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
             case ('meat'):
-                ingredient = <div className='classes.Meat'></div>;
+                ingredient = <div className={classes.Meat}></div>;
                 break;
             case ('cheese'):
-                ingredient = <div className='classes.Cheese'></div>;
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
             case ('salad'):
-                ingredient = <div className='classes.Salad'></div>;
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             case ('bacon'):
-                ingredient = <div className='classes.Bacon'></div>;
+                ingredient = <div className={classes.Bacon}></div>;
                 break;
             default:
                 ingredient = null;
@@ -39,7 +40,7 @@ class BurgerIngredient extends Component {
 }
 
 BurgerIngredient.propTypes = {
-    type: PropTypes.string.isRequired;
+    type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient;
