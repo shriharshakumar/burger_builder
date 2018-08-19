d9d474656cdae4729294df9b09301c223a67db47
shriharshakumar <shriharshakumar@gmail.com>
2018-08-19 10:00:21 +0530

    Added custom ingredients...

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,24 @@ import Burger from '../../components/Burger/Burger'
 import Aux from '../../hoc/Auxiliary';
 
 class BurgerBuilder extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            ingredients: {
+                cheese: 2,
+                salad: 2,
+                bacon: 1,
+                meat: 1
+            }
+        }
+    }
+
     render(){
         return (
             <Aux>
-                <Burger />    
+                <Burger ingredients={this.state.ingredients}/>    
                 <div>Build Controls</div>
             </Aux>
         )
