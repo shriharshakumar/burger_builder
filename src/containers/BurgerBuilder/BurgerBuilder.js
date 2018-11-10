7517688e6a78f5dce7a86b867fb722a9e8fc0710
shriharshakumar <shriharshakumar@gmail.com>
2018-11-10 16:40:45 +0530

    Added get ingredients from database...

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,17 +22,23 @@ class BurgerBuilder extends Component {
         super(props);
 
         this.state = {
-            ingredients: {
-                cheese: 0,
-                salad: 0,
-                bacon: 0,
-                meat: 0
-            },
+            ingredients: null,
             totalPrice: 4,
             purchasable: false,
             purchasing: false,
-            loading: false
+            loading: false,
+            error: false
         }
+    }
+
+    componentDidMount () {
+        axios.get('/ingredients.json')
+        .then(response => {
+            this.setState({ingredients: response.data});
+        })
+        .catch(error => {
+            this.setState({error: true});
+        });
     }
 
     updatePurchaseState (ingredients) {
@@ -130,26 +136,36 @@ class BurgerBuilder extends Component {
         for(let key in disabled){
             disabled[key] = disabled[key] <= 0
         }
-        let orderSummary = <OrderSummary
-            ingredients={this.state.ingredients}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+        let orderSummary = <Spinner />
         if(this.state.loading){
             orderSummary = <Spinner />
+        }
+        let burger = this.state.error ? "Something went wrong !" : <Spinner />;
+        if(this.state.ingredients){
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients} />
+                    <BurgerControls
+                        ingredientsAdded={this.addIngredientsHandler}
+                        ingredientsRemoved={this.removeIngredientsHandler}
+                        disabled={disabled}
+                        price={this.state.totalPrice}
+                        ordered={this.purchaseHandler}
+                        purchasable={this.state.purchasable} />
+                </Aux>
+
+            );
+            orderSummary = <OrderSummary
+                ingredients={this.state.ingredients}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler} />
         }
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>    
-                <BurgerControls 
-                    ingredientsAdded={this.addIngredientsHandler}
-                    ingredientsRemoved={this.removeIngredientsHandler}
-                    disabled = {disabled}
-                    price = {this.state.totalPrice}
-                    ordered={this.purchaseHandler}
-                    purchasable = {this.state.purchasable} />
+                {burger}
             </Aux>
         )
     }
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,14 +8,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
         state = {
             error: null
         }
-        componentDidMount() {
-            axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
+        componentWillMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
+            this.setState({error: null});
+            return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
+        }
+
+        componentWillUnmount() {
+            console.log("Will unmount!");
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         closedErrorHandler = () => {
