6f623be030b02c30d7185d99759d82249b1bdf34
shriharshakumar <shriharshakumar@gmail.com>
2019-01-09 11:13:23 +0530

    added redux thunk and action types...

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
     return{
@@ -12,4 +13,32 @@ export const removeIngredient = (name) => {
         type: actionTypes.REMOVE_INGREDIENT,
         ingredientName: name
     };
+};
+
+export const initIngredients = (ingredients) => {
+    return dispatch => {
+        console.log(this.props);
+        axios.get('/ingredients.json')
+            .then(response => {
+                dispatch(setIngredients(response.data));
+            })
+            .catch(error => {
+                actionTypes.FAILED_LOADING_INGREDIENTS
+            });
+    }
+};
+
+export const setIngredients = (order) => {
+    axios.post('/orders.json', order)
+        .then(response => {
+            this.setState({
+                loading: false,
+            });
+            this.props.history.push("/");
+        })
+        .catch(error => {
+            this.setState({
+                loading: false,
+            })
+        });
 };
